3bcc36a0ea8e09108f07c28304895f976f493ea8
wemlaztdj <wemlhui@gmail.com>
2023-10-08 17:23:24 -0400

    matadata problem

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,11 @@
 var url ='https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-var barchart
+var chart;
+var matadata;
 d3.json(url).then(function(data) {
     console.log(data);
-    barchart= data.samples
-
+    chart= data.samples
+    matadata= data.metadata
+    console.log(matadata);
     var select = document.getElementById('selDataset');
     select.innerHTML = '';
 
@@ -17,6 +19,7 @@ d3.json(url).then(function(data) {
         select.add(option);
       }
 
+      optionChanged('940');
 
 
   });
@@ -25,12 +28,59 @@ d3.json(url).then(function(data) {
   function optionChanged(selectedValue) {
     console.log('Selected Value:', selectedValue);
     //update the bar chart
+      barchart(selectedValue);
+      bubblechart(selectedValue);
+      mata(selectedValue);
+
+  }
+
+  function searchById(theid) {
+    console.log(chart);
+    return chart.filter(item=> item.id===theid)[0]
+  }
+  function searchmataById(theid) {
+    console.log(matadata);
+    return matadata.filter(item=> item.id===theid)[0]
+  }
+
+  function barchart(selectedValue){
     var result = searchById(selectedValue); 
-    console.log('Selected Value:', result);
+    
+    
+    
+    console.log(result);
+
+
+    var ids = result.otu_ids.slice(0,10)
+    var values = result.sample_values.slice(0,10)
+    var lables= result.otu_labels.slice(0,10)
+    
+    console.log('Selected ids:', ids);
+    console.log('Selected values:', values);
+    console.log('Selected lables:', lables);
+
+    var barlist=[]
+    for (var i=0;i<ids.length;i++){
+      barlist.push(
+        {
+          'ids':ids[i],
+          'values':values[i],
+          'lables':lables[i]
+
+        }
+      )
+
+    }    
+    var sortedbarlist=barlist.sort((a,b)=>a.values-b.values)
+    
     var trace = {
-        x:  result.map(row => row.sample_values).slice(0, 5),
+        x:  sortedbarlist.map(item=>item.values),
         
-        y: 'OTU '+ result.result.map(row => row.otu_ids).slice(0, 5), 
+        y: sortedbarlist.map(item=>`OTU ${item.ids}`), 
+
+        hovertext: sortedbarlist.map(item=>item.lables),
+
+
         type: 'bar',
         orientation: 'h'  // This makes the bar chart horizontal
       };
@@ -39,10 +89,80 @@ d3.json(url).then(function(data) {
       // Render the chart
       Plotly.newPlot('bar', [trace]);
 
+
+
+
+
   }
 
-  function searchById(id) {
-    return barchart.id === id
-}
 
- // Searches for the object with id '941'
+  function bubblechart(selectedValue){
+    var result = searchById(selectedValue);
+    var trace1 = {
+      x: result.otu_ids,
+      y: result.sample_values,
+      mode: 'markers',
+      marker: {
+        size: result.sample_values,
+        color: result.otu_ids
+      }
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+      title: result.otu_labels,
+      showlegend: false,
+      height: 600,
+      width: 1500
+    };
+    
+    Plotly.newPlot('bubble', data, layout);
+  }
+
+  function mata(selectedValue){
+
+    var mataresult = searchmataById(selectedValue);
+    var table = d3.select("sample-metadata");
+    table.html("");
+    console.log('mataresult:'+mataresult);
+
+
+    var row = table.append("tr");
+    
+   
+      row.append("td").text("id");
+      row.append("td").text(result.id);
+     
+    row = table.append("tr");
+
+      row.append("td").text("ethnicity");
+      row.append("td").text(result.ethnicity);
+     
+    row = table.append("tr");
+      
+      row.append("td").text("gender");
+      row.append("td").text(result.gender);
+     
+    row = table.append("tr");
+      
+      row.append("td").text("age");
+      row.append("td").text(result.age);
+     
+    row = table.append("tr");
+      
+      row.append("td").text("location");
+      row.append("td").text(result.location);
+     
+    row = table.append("tr");
+      
+      row.append("td").text("bbtype");
+      row.append("td").text(result.bbtype);
+     
+    row = table.append("tr");
+      
+      row.append("td").text("wfreq");
+      row.append("td").text(result.wfreq);
+     
+  
+  }
